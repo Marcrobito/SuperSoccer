bde7e41f88e9ff173f14a8ad846272746856f4a2
Marco Antonio Martinez <gutierrezma@ae.com>
2025-06-10 13:06:00 -0600

    uodated

--- Assets/Scripts/GameStartedState.cs
+++ Assets/Scripts/GameStartedState.cs
@@ -7,16 +7,6 @@ public class GameStartedState : GameState
     [SerializeField]
     private GameObject mainPanel;
 
-    [SerializeField] private ImageSwitcher imageSwitcher;
-    [SerializeField] private TMP_Dropdown imageSelector;
-
-    void Start()
-    {
-        imageSelector.gameObject.SetActive(true);
-        imageSelector.onValueChanged.AddListener(OnDropdownValueChanged);
-    }
-
-
     public override IEnumerator EnterState()
     {
         mainPanel.SetActive(true);
@@ -25,25 +15,7 @@ public class GameStartedState : GameState
 
     public void onStartClicked(){
         mainPanel.SetActive(false);
-        imageSelector.gameObject.SetActive(false);
         OnStateCompleted?.Invoke(-1);
-    }
-
-    private void OnDropdownValueChanged(int index)
-    {
-        Debug.Log($"index {index}");
-        switch (index)
-        {
-            case 0:
-                imageSwitcher.SetImageA();
-                break;
-            case 1:
-                imageSwitcher.SetImageB();
-                break;
-            case 2:
-                imageSwitcher.ClearImage();
-                break;
-        }
     }
 
 }
